4e4acfb6cce9f5bd808741ca9bcbaa7b01813707
Asion Solver <asionsolver274@gmail.com>
2021-07-14 00:43:04 +0600

    Add CallByReferencecd /mnt/f/GitCode/C++/C-NoteArguments.cc file

---
+++ CallByReferenceArguments.cc
@@ -0,0 +1,40 @@
+#include<bits/stdc++.h>
+using namespace std;
+
+//This will not swap a and b
+void swap(int a, int b){  // a and b is called formall arguments
+    int temp = a;
+    a = b;
+    b = temp;
+}
+//Call-by-reference with reference argument (&-->pointer arguments)
+//This will reference argument swap a and b
+void swapReference(int &a, int &b){  // a and b is called formall arguments
+    int temp = a;
+    a = b;
+    b = temp;
+}
+
+int main(){
+    int x = 45, y = 54;
+    cout<<"Before Swaping:"<<endl;
+    cout<<"The value of x is "<<x<<endl;
+    cout<<"The value of y is "<<y<<endl<<endl;
+
+    //This will not swap a and b
+    swap(x, y); //x and y is called actual arguments
+
+    //This will not swap a and b
+    cout<<"After Swaping:"<<endl;
+    cout<<"The value of x is "<<x<<endl; //do not swapping because swap function nothing is returning
+    cout<<"The value of y is "<<y<<endl<<endl; //do not swapping because swap function nothing is returning //This will not swap a and b
+    
+    swapReference(x, y); //x and y is called actual arguments
+    
+    //htis will swap a and b
+    cout<<"After Swaping:"<<endl;
+    cout<<"The value of x is "<<x<<endl; // swapping because I gave the Reference of x and y in swapReference function
+    cout<<"The value of y is "<<y<<endl<<endl; // swapping because I gave the Reference of x and y in swapReference function
+
+    return 0;
+}
